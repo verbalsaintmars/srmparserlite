713f652e0b069fcb6fef49cd82e1e897dfcf8e10
verbalsaintmars <11008huan.tech@gmail.com>
2013-10-21 23:36:18 -0700

    start script:srmparser.py

--- srmparser.py
+++ srmparser.py
@@ -1,1 +1,15 @@
- 
+import srmparserlite.spl as spl
+import srmparserlite.splConfig.configpack as configpack
+import srmparserlite.splTraits.filefmt as filefmt
+import pprint
+import os
+
+
+l_sites = configpack.ReadYaml(
+   os.path.join(
+      os.getcwd(),
+      filefmt.DefaultYamlFile())).LoadYaml()
+
+pprint.pprint(l_sites)
+sp = spl.Start()
+sp.Start(l_sites)
